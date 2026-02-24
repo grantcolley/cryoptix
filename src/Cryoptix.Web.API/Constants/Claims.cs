09f5748e7e50b954865324cb1f3a6fb9c2ebe399
Grant Colley <grantcolley@yahoo.com>
2026-02-24 21:46:57 +0000

    Create Cryoptix.Web.API project

---
+++ src/Cryoptix.Web.API/Constants/Claims.cs
@@ -0,0 +1,9 @@
+﻿namespace Cryoptix.Web.API.Constants
+{
+    public static class Claims
+    {
+        public const string CRYOPTIX_CLIENT_ID = "cryoptix-client-id";
+        public const string CRYOPTIX_USER_CLAIM = "read:cryoptix-user";
+        public const string CRYOPTIX_DEVELOPER_CLAIM = "read:cryoptix-developer";
+    }
+}
