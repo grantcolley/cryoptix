9436cf3823765d1222eab268a06fb756bc1c3dc4
Grant Colley <grantcolley@yahoo.com>
2026-03-26 08:27:24 +0000

    Update IExchangeSubscriptionApi.cs

--- src/api/Cryoptix.Exchange/Api/IExchangeSubscriptionApi.cs
+++ src/api/Cryoptix.Exchange/Api/IExchangeSubscriptionApi.cs
@@ -5,10 +5,10 @@ namespace Cryoptix.Exchange.Api
     public interface IExchangeSubscriptionApi
     {
         Exchanges.Exchange Exchange { get; }
-        Task<IAsyncDisposable> SubscribeToAccountUpdatesAsync(Credentials user, Action<AccountEventArgs> callback, Action<Exception> onError, CancellationToken cancellationToken);
-        Task<IAsyncDisposable> SubscribeToKlineUpdatesAsync(string symbol, KlineInterval interval, Action<KlineEventArgs> callback, Action<Exception> onError, CancellationToken cancellationToken);
-        Task<IAsyncDisposable> SubscribeToOrderBookAsync(string symbol, int? limit, Action<OrderBookEventArgs> callback, Action<Exception> onError, CancellationToken cancellationToken);
-        Task<IAsyncDisposable> SubscribeToSymbolStatisticsAsync(IEnumerable<string> symbols, Action<StatisticsEventArgs> callback, Action<Exception> onError, CancellationToken cancellationToken);
-        Task<IAsyncDisposable> SubscribeToTradesAsync(string symbol, Action<TradeEventArgs> callback, Action<Exception> onError, CancellationToken cancellationToken);
+        Task<IAsyncDisposable> SubscribeToAccountUpdatesAsync(Credentials user, Action<AccountEventArgs> onCallback, Action<Exception> onError, CancellationToken cancellationToken);
+        Task<IAsyncDisposable> SubscribeToKlineUpdatesAsync(string symbol, KlineInterval interval, Action<KlineEventArgs> onCallback, Action<Exception> onError, CancellationToken cancellationToken);
+        Task<IAsyncDisposable> SubscribeToOrderBookAsync(string symbol, int? limit, Action<OrderBookEventArgs> onCallback, Action<Exception> onError, CancellationToken cancellationToken);
+        Task<IAsyncDisposable> SubscribeToSymbolStatisticsAsync(IEnumerable<string> symbols, Action<StatisticsEventArgs> onCallback, Action<Exception> onError, CancellationToken cancellationToken);
+        Task<IAsyncDisposable> SubscribeToTradesAsync(string symbol, Action<TradeEventArgs> onCallback, Action<Exception> onError, CancellationToken cancellationToken);
     }
 }
