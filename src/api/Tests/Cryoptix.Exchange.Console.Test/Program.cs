eaac2aaa6039e23a7170a96a80ab0827b3e7752e
Grant Colley <grantcolley@yahoo.com>
2026-03-29 21:39:37 +0100

    Update Program.cs

--- src/api/Tests/Cryoptix.Exchange.Console.Test/Program.cs
+++ src/api/Tests/Cryoptix.Exchange.Console.Test/Program.cs
@@ -29,7 +29,7 @@ ClientOrder clientOrder = new()
 #pragma warning disable CS0219 // Variable is assigned but its value is never used
 string btcUsdc = "BTCUSDC";
 string ethUSDT = "ETHUSDT";
-int? limit = 100;
+int? limit = null;
 int? level = 10;
 int? recWindow = null;
 #pragma warning restore CS0219 // Variable is assigned but its value is never used
@@ -39,18 +39,18 @@ TestExchangeRestApi exchangeRestApi = new(new BinanceRestApi(new BinanceRestClie
 //_ = await exchangeRestApi.GetAccountInfoAsync(ct);
 //_ = await exchangeRestApi.GetOpenOrdersAsync(btcUsdc, recWindow, ct);
 //_ = await exchangeRestApi.PlaceOrderAsync(clientOrder, recWindow, ct);
-//_ = await exchangeRestApi.GetKlinesAsync(btcUsdc, KlineInterval.Minute, DateTime.Now.Subtract(new TimeSpan(1, 0, 0)), DateTime.Now, limit, ct);
+_ = await exchangeRestApi.GetKlinesAsync(btcUsdc, KlineInterval.Minute, DateTime.Now.Subtract(new TimeSpan(8, 0, 0)), DateTime.Now, limit, ct);
 //_ = await exchangeRestApi.GetOrderBookAsync(btcUsdc, limit, ct);
 //_ = await exchangeRestApi.GetSymbolsAsync(ct);
-_ = await exchangeRestApi.GetTradesAsync(btcUsdc, limit, ct);
+//_ = await exchangeRestApi.GetTradesAsync(btcUsdc, limit, ct);
 exchangeRestApi.Dispose();
 
 TestExchangeSubscriptionApi exchangeSubscriptionApi = new(new BinanceSubscriptionApi());
 //await using IAsyncDisposable subscribeAccount = await exchangeSubscriptionApi.SubscribeToAccountUpdatesAsync(credentials, o => { }, e => { }, ct);
-//await using IAsyncDisposable subscribeKlines = await exchangeSubscriptionApi.SubscribeToKlineUpdatesAsync(btcUsdc, KlineInterval.Minute, k => { }, e => { }, ct);
+await using IAsyncDisposable subscribeKlines = await exchangeSubscriptionApi.SubscribeToKlineUpdatesAsync(btcUsdc, KlineInterval.Minute, k => { }, e => { }, ct);
 //await using IAsyncDisposable subscribeOrderBook = await exchangeSubscriptionApi.SubscribeToOrderBookAsync(btcUsdc, level, o => { }, e => { }, ct);
 //await using IAsyncDisposable subscribeStatistics = await exchangeSubscriptionApi.SubscribeToSymbolStatisticsAsync([btcUsdc, ethUSDT], s => { }, e => { }, ct);
-await using IAsyncDisposable subscribeTrades = await exchangeSubscriptionApi.SubscribeToTradesAsync(btcUsdc, t => { }, e => { }, ct);
+//await using IAsyncDisposable subscribeTrades = await exchangeSubscriptionApi.SubscribeToTradesAsync(btcUsdc, t => { }, e => { }, ct);
 
 _ = Console.ReadLine();
 
