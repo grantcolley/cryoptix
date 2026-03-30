f74dc70f8b19c68e89fe0607d22c16cb5b368328
Grant Colley <grantcolley@yahoo.com>
2026-03-30 07:55:30 +0100

    Update IExchangeRestApi.cs

--- src/api/Cryoptix.Exchange/Api/IExchangeRestApi.cs
+++ src/api/Cryoptix.Exchange/Api/IExchangeRestApi.cs
@@ -9,7 +9,7 @@ namespace Cryoptix.Exchange.Api
         Task<Order> PlaceOrderAsync(ClientOrder clientOrder, int? recWindow, CancellationToken cancellationToken = default);
         Task<List<Order>> GetOpenOrdersAsync(string symbol, int? recWindow, CancellationToken cancellationToken = default);
         Task<string> CancelOrderAsync(string symbol, string orderId, CancellationToken cancellationToken = default);
-        Task<List<Kline>> GetKlinesAsync(string symbol, KlineInterval interval, DateTime startTime, DateTime endTime, int? limit, CancellationToken token = default);
+        Task<List<Kline>> GetKlinesAsync(string symbol, KlineInterval interval, DateTime startTime, DateTime endTime, int? limit, CancellationToken cancellationToken = default);
         Task<OrderBook> GetOrderBookAsync(string symbol, int? limit, CancellationToken cancellationToken = default);
         Task<List<Symbol>> GetSymbolsAsync(CancellationToken cancellationToken);
         Task<List<Trade>> GetTradesAsync(string symbol, int? limit, CancellationToken cancellationToken);
