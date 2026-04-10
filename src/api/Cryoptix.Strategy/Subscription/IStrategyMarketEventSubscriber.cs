9be746cb0c5b1811feed72898aa062faa2073817
Grant Colley <grantcolley@yahoo.com>
2026-04-10 13:46:42 +0100

    Create IStrategyMarketEventSubscriber.cs

---
+++ src/api/Cryoptix.Strategy/Subscription/IStrategyMarketEventSubscriber.cs
@@ -0,0 +1,15 @@
+﻿using Cryoptix.Exchange.Api;
+using Cryoptix.Strategy.Event;
+using System.Threading.Channels;
+
+namespace Cryoptix.Strategy.Subscription
+{
+    public interface IStrategyMarketEventSubscriber
+    {
+        Task<StrategyMarketEventSubscriptions> SubscribeAsync(
+            Runtime.Strategy strategy,
+            IExchangeSubscriptionApi subscriptionsApi,
+            ChannelWriter<MarketEvent> writer,
+            CancellationToken cancellationToken);
+    }
+}
