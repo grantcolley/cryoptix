c60bae66a658ad29942c611b495aa1783263fb72
Grant Colley <grantcolley@yahoo.com>
2026-04-10 13:46:53 +0100

    Create IStrategyMarketEventDispatcher.cs

---
+++ src/api/Cryoptix.Strategy/Dispatcher/IStrategyMarketEventDispatcher.cs
@@ -0,0 +1,13 @@
+﻿using Cryoptix.Strategy.Event;
+using Cryoptix.Strategy.Processor;
+
+namespace Cryoptix.Strategy.Dispatcher
+{
+    public interface IStrategyMarketEventDispatcher
+    {
+        Task DispatchAsync(
+            StrategyProcessorSession session,
+            MarketEvent marketEvent,
+            CancellationToken cancellationToken);
+    }
+}
