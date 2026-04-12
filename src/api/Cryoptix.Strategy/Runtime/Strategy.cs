81637caa06721daaff536e34cbfa9adea99d9c76
Grant Colley <grantcolley@yahoo.com>
2026-04-12 10:32:56 +0100

    Update Strategy.cs

--- src/api/Cryoptix.Strategy/Runtime/Strategy.cs
+++ src/api/Cryoptix.Strategy/Runtime/Strategy.cs
@@ -1,5 +1,6 @@
 ﻿using Cryoptix.Exchange.Models;
 using Cryoptix.Strategy.Processor;
+using System.Threading.Channels;
 
 namespace Cryoptix.Strategy.Runtime
 {
@@ -9,11 +10,19 @@ namespace Cryoptix.Strategy.Runtime
         public string? Name { get; set; }
         public string? Description { get; set; }
         public string? Symbol { get; set; }
-        public KlineInterval KlineInterval { get; set; }
-        public StrategyProcessorType StrategyProcessorType { get; set; }
-        public StrategyEngineType StrategyEngineType { get; set; }
-        public Exchange.Exchanges.Exchange Exchange { get; set; }
+        public KlineInterval KlineInterval { get; set; } = KlineInterval.Minute;
+        public StrategyProcessorType StrategyProcessorType { get; set; } = StrategyProcessorType.TradingFlow;
+        public StrategyEngineType StrategyEngineType { get; set; } = StrategyEngineType.None;
+        public Exchange.Exchanges.Exchange Exchange { get; set; } = Cryoptix.Exchange.Exchanges.Exchange.Binance;
         public int FastPeriod { get; init; }
         public int SlowPeriod { get; init; }
+
+        public int CacheMaxKlinesPerSeries { get; set; } = 5000;
+        public int CacheMaxTradesPerSymbol { get; set; } = 10000;
+        public int StrategyProcessorMaxTradesPerPass { get; set; } = 256;
+        public int SubscriptionChannelKlineCapacity { get; set; } = 500;
+        public int SubscriptionChannelTradeCapacity { get; set; } = 10000;
+        public bool SubscriptionChannelDropTradesWhenFull { get; set; } = true;
+        public BoundedChannelFullMode SubscriptionChannelKlineFullMode { get; set; } = BoundedChannelFullMode.Wait;
     }
 }
