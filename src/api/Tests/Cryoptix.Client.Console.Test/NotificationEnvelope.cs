9c31b2005ad603b188ab4efbee3283c28fa2e88e
Grant Colley <grantcolley@yahoo.com>
2026-04-21 20:48:34 +0100

    Create NotificationEnvelope.cs

---
+++ src/api/Tests/Cryoptix.Client.Console.Test/NotificationEnvelope.cs
@@ -0,0 +1,9 @@
+﻿namespace Cryoptix.Client.Console.Test
+{
+    public sealed class NotificationEnvelope
+    {
+        public string MessageType { get; set; } = default!;
+        public DateTime TimestampUtc { get; set; }
+        public string Payload { get; set; } = default!;
+    }
+}
