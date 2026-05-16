69e91efa34a23880d940cf71040909ac199a63e4
Grant Colley <grantcolley@yahoo.com>
2026-05-16 12:56:16 +0100

    Update strategy-page.tsx

--- src/api/Cryoptix.Web.API/Program.cs
+++ src/api/Cryoptix.Web.API/Program.cs
@@ -94,6 +94,22 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidAudience = audience,
             NameClaimType = "sub",
         };
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                var accessToken = context.Request.Query["access_token"];
+                var path = context.HttpContext.Request.Path;
+
+                if (!string.IsNullOrEmpty(accessToken) &&
+                    path.StartsWithSegments("/api/strategy/subscribe"))
+                {
+                    context.Token = accessToken;
+                }
+
+                return Task.CompletedTask;
+            }
+        };
     });
 
 builder.Services.AddAuthorizationBuilder()
