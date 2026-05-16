2ede741778ed9317bc6bbca31a6242fbdc4149be
Grant Colley <grantcolley@yahoo.com>
2026-05-16 19:53:33 +0100

    Update home-page.tsx

--- src/gui/cryoptix/src/pages/home-page.tsx
+++ src/gui/cryoptix/src/pages/home-page.tsx
@@ -2,7 +2,7 @@ import { CryoptixLogo } from "@/components/icon/CryoptixLogo";
 
 export function HomePage() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-background">
+    <main className="flex flex-1 items-center justify-center bg-background">
       <div className="flex h-96 w-96 items-center justify-center">
         <CryoptixLogo variant="logo" className="h-full w-full object-contain" />
       </div>
