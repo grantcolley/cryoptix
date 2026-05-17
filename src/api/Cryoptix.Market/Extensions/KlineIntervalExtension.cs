efa75d7c53f248f2f9cfa1cf1a711a8a16bbc795
Grant Colley <grantcolley@yahoo.com>
2026-05-17 21:01:08 +0100

    Create KlineIntervalExtension.cs

---
+++ src/api/Cryoptix.Market/Extensions/KlineIntervalExtension.cs
@@ -0,0 +1,30 @@
+﻿using Cryoptix.Market.Data;
+
+namespace Cryoptix.Market.Extensions
+{
+    public static class KlineIntervalExtension
+    {
+        public static  int KlineIntervalToMinutes(this KlineInterval interval)
+        {
+            return interval switch
+            {
+                KlineInterval.Minute => 1,
+                KlineInterval.Minutes3 => 3,
+                KlineInterval.Minutes5 => 5,
+                KlineInterval.Minutes15 => 15,
+                KlineInterval.Minutes30 => 30,
+                KlineInterval.Hour => 60,
+                KlineInterval.Hours2 => 120,
+                KlineInterval.Hours4 => 240,
+                KlineInterval.Hours6 => 360,
+                KlineInterval.Hours8 => 480,
+                KlineInterval.Hours12 => 720,
+                KlineInterval.Day => 1440,
+                KlineInterval.Days3 => 4320,
+                KlineInterval.Week => 10080,
+                KlineInterval.Month => 43200,
+                _ => throw new ArgumentOutOfRangeException(nameof(interval), $"Not expected interval value: {interval}"),
+            };
+        }
+    }
+}
