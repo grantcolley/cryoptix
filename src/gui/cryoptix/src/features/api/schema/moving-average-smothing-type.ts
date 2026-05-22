51446e01cdc3b49242bfe0c17a96006aae9d4e49
Grant Colley <grantcolley@yahoo.com>
2026-05-22 08:32:25 +0100

    Create moving-average-smothing-type.ts

---
+++ src/gui/cryoptix/src/features/api/schema/moving-average-smothing-type.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export const MovingAverageSmoothingType = {
+  Sma: 0,
+  Ema: 1,
+} as const;
+
+export const MovingAverageSmoothingTypeSchema = z.enum(
+  MovingAverageSmoothingType
+);
+
+export type MovingAverageSmoothingType = z.infer<
+  typeof MovingAverageSmoothingTypeSchema
+>;
+
+export const ExchangeLabels: Record<MovingAverageSmoothingType, string> = {
+  [MovingAverageSmoothingType.Sma]: "SMA",
+  [MovingAverageSmoothingType.Ema]: "EMA",
+};
