f527cad501da66b284f37ac65cccc35f5a7f1bef
Grant Colley <grantcolley@yahoo.com>
2026-05-22 08:17:37 +0100

    Create MovingAverageKeys.cs

---
+++ src/api/Cryoptix.Strategy/Engine/MovingAverageKeys.cs
@@ -0,0 +1,10 @@
+﻿namespace Cryoptix.Strategy.Engine
+{
+    public static class MovingAverageKeys
+    {
+        public const string SmaFast = "SMA_FAST";
+        public const string SmaSlow = "SMA_SLOW";
+        public const string EmaFast = "EMA_FAST";
+        public const string EmaSlow = "EMA_SLOW";
+    }
+}
