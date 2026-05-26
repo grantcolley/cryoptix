dd70f1caace8f1ddfb594aa2807db1da8dbf5efd
Grant Colley <grantcolley@yahoo.com>
2026-05-26 07:51:55 +0100

    Update INotificationPump.cs

--- src/api/Cryoptix.Strategy/Notification/INotificationPump.cs
+++ src/api/Cryoptix.Strategy/Notification/INotificationPump.cs
@@ -7,9 +7,10 @@ namespace Cryoptix.Strategy.Notification
         /// <summary>
         /// Starts the notification pump which reads events from the provided channels and publishes notifications.
         /// </summary>
+        /// <param name="strategy">The strategy for which to publish notifications.</param>
         /// <param name="channels">The strategy event channels to read from.</param>
         /// <param name="cancellationToken">A token to observe while waiting for the pump to finish.</param>
         /// <returns>A task that completes when the pump stops.</returns>
-        Task RunAsync(StrategyEventChannels channels, CancellationToken cancellationToken);
+        Task RunAsync(Strategies.Strategy strategy, StrategyEventChannels channels, CancellationToken cancellationToken);
     }
 }
