8222e2c4575221c03617d55e70229f1fa480431f
Grant Colley <grantcolley@yahoo.com>
2026-06-16 14:08:30 +0100

    Update MapCryoptixEndpoints.cs

--- src/api/Cryoptix.Web.API/Endpoints/MapCryoptixEndpoints.cs
+++ src/api/Cryoptix.Web.API/Endpoints/MapCryoptixEndpoints.cs
@@ -6,10 +6,12 @@ namespace Cryoptix.Web.API.Endpoints
     {
         public static IEndpointRouteBuilder MapCryoptixApi(this IEndpointRouteBuilder app)
         {
+            app.MapGet("/", () => Results.Ok($"Cryoptix API running {DateTime.Now}"));
+            app.MapGet("/health", () => Results.Ok($"Cryoptix is Healthy {DateTime.Now}"));
+
             var apiGroup = app.MapGroup("/api")
                 .RequireAuthorization(Claims.CRYOPTIX_CLIENT_ID, Claims.CRYOPTIX_USER_CLAIM);// group-wide policy
 
-            apiGroup.MapAppApi();
             apiGroup.MapStrategyApi();
 
             return app;
