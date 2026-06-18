94975b172e18ae0e178aec74d7fb2afb9f604608
Grant Colley <grantcolley@yahoo.com>
2026-06-18 19:29:41 +0100

    Delete obsolete test projects

--- src/api/Tests/Cryoptix.Client.Console.Test/NotificationEnvelope.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Cryoptix.Client.Console.Test
-{
-    public sealed class NotificationEnvelope
-    {
-        public string MessageType { get; set; } = default!;
-        public DateTime TimestampUtc { get; set; }
-        public object? Payload { get; set; } = default!;
-    }
-}
